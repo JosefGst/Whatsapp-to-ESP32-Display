de9b8ec4d161a77476ee0fb6cadc58a2172fcbee
JosefGst <josefgstoettner@live.at>
2025-04-28 12:18:26 +0800

    add millis and time

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <MD_MAX72xx.h>
 #include <SPI.h>
 #include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
+#include <time.h>
 
 ThingESP32 thing("josefgst", "espdisplay", "espdisplayforkit");
 
@@ -24,11 +25,27 @@ bool deisplayOn = true;
 // Hardware SPI connection
 MD_Parola P = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
 
+// TIME
+const char* ntpServer = "pool.ntp.org";
+const long gmtOffset_sec = 8*3600; // GMT+8
+const int daylightOffset_sec = 0;
+
+// MILLIS
+unsigned long startMillis; //some global variables available anywhere in the program
+unsigned long currentMillis;
+const unsigned long period = 10*1000; //the value is a number of milliseconds
+
+void printLocalTime();
+
+
+
 void setup()
 {
   Serial.begin(115200);
 
   pinMode(LED, OUTPUT);
+
+  startMillis = millis();
 
   P.begin();
 
@@ -57,6 +74,9 @@ void setup()
   // comment this line because wifi connection is done by WiFiManager
   // thing.SetWiFi("AndroidAP", "csw2tpz;");
   thing.initDevice();
+
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  printLocalTime();
 }
 
 String HandleResponse(String query)
@@ -127,8 +147,11 @@ String HandleResponse(String query)
 
 void loop()
 {
-
+  currentMillis = millis();
+  
   thing.Handle();
+
+  
 
   if (deisplayOn)
   {
@@ -140,5 +163,50 @@ void loop()
   {
     P.displayClear();
   }
+
+  if (currentMillis - startMillis >= period)
+  {
+    startMillis = currentMillis; // IMPORTANT to save the start time of the current LED state.
+    printLocalTime();
+  }
+
 }
 // void loop()
+
+
+
+// HELPER FUNCTIONS
+
+void printLocalTime(){
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("Failed to obtain time");
+    return;
+  }
+  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+  Serial.print("Day of week: ");
+  Serial.println(&timeinfo, "%A");
+  Serial.print("Month: ");
+  Serial.println(&timeinfo, "%B");
+  Serial.print("Day of Month: ");
+  Serial.println(&timeinfo, "%d");
+  Serial.print("Year: ");
+  Serial.println(&timeinfo, "%Y");
+  Serial.print("Hour: ");
+  Serial.println(&timeinfo, "%H");
+  Serial.print("Hour (12 hour format): ");
+  Serial.println(&timeinfo, "%I");
+  Serial.print("Minute: ");
+  Serial.println(&timeinfo, "%M");
+  Serial.print("Second: ");
+  Serial.println(&timeinfo, "%S");
+
+  Serial.println("Time variables");
+  char timeHour[3];
+  strftime(timeHour,3, "%H", &timeinfo);
+  Serial.println(timeHour);
+  char timeWeekDay[10];
+  strftime(timeWeekDay,10, "%A", &timeinfo);
+  Serial.println(timeWeekDay);
+  Serial.println();
+}
